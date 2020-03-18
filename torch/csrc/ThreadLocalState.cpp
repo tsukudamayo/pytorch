e0ec672623c5d1850f3ac7749a032fb9d1bb305f
pritam <pritam.damania@fb.com>
2020-03-18 11:49:08 -0700

    Respect dist autograd context in torch.jit._fork.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34360
    
    The distributed autograd context sets up a thread local context id
    which is used to perform appropriate book keeping and autograd recording of RPC
    functions in the forward pass.
    
    However, if we use torch.jit._fork within the distributed autograd context, the
    code executed within torch.jit._fork will lose this context since it is run in
    a separate JIT thread and the thread local is not set in that thread.
    
    To fix this problem, we pass in the distributed autograd context to
    torch.jit._fork similar to what we did in
    https://github.com/pytorch/pytorch/pull/16101.
    ghstack-source-id: 100398773
    
    Differential Revision: [D20301352](https://our.internmc.facebook.com/intern/diff/D20301352/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D20301352/)!

---
+++ torch/csrc/ThreadLocalState.cpp
@@ -0,0 +1,37 @@
+#include <torch/csrc/autograd/grad_mode.h>
+#ifdef USE_DISTRIBUTED
+#include <torch/csrc/distributed/autograd/context/container.h>
+#endif
+#include <torch/csrc/ThreadLocalState.h>
+
+namespace torch {
+
+#ifdef USE_DISTRIBUTED
+using torch::distributed::autograd::DistAutogradContainer;
+#endif
+
+ThreadLocalState::ThreadLocalState(
+    bool grad_mode_enabled,
+    int64_t dist_autograd_context_id)
+    : grad_mode_enabled_(grad_mode_enabled),
+      dist_autograd_context_id_(dist_autograd_context_id) {}
+
+ThreadLocalState ThreadLocalState::getThreadLocalState() {
+  int64_t dist_autograd_context_id = -1;
+#ifdef USE_DISTRIBUTED
+  dist_autograd_context_id = DistAutogradContainer::currentContextId();
+#endif
+
+  return ThreadLocalState(
+      autograd::GradMode::is_enabled(), dist_autograd_context_id);
+}
+
+void ThreadLocalState::setThreadLocalState(const ThreadLocalState& state) {
+  at::GradMode::set_enabled(state.grad_mode_enabled_);
+
+#ifdef USE_DISTRIBUTED
+  DistAutogradContainer::forceCurrentContextId(state.dist_autograd_context_id_);
+#endif
+}
+
+} // namespace torch
